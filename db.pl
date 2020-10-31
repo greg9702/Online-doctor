414f0b36075e77d82c0ccf821e0af6e32016da5f
greg9702 <grzegorzb9702@gmail.com>
2020-10-31 17:52:05 +0100

    First version using start

--- db.pl
+++ db.pl
@@ -1,43 +1,71 @@
-podejrzenie(Pacjent,grypa) :-
-        ma_objaw(Pacjent,goraczka),
-        ma_objaw(Pacjent,bol_gardla),
-        ma_objaw(Pacjent,bol_glowy),
-        ma_objaw(Pacjent,bol_miesni),
-        ma_objaw(Pacjent,dreszcze),
-        ma_objaw(Pacjent,katar),
-        ma_objaw(Pacjent,kaszel).
+:- dynamic
+    xpozytywne/1,
+    xnegatywne/1.
 
-podejrzenie(Pacjent,przeziebienie) :-
-        ma_objaw(Pacjent,bol_glowy),
-        ma_objaw(Pacjent,bol_gardla),
-        ma_objaw(Pacjent,kichanie),
-        ma_objaw(Pacjent,katar),
-        ma_objaw(Pacjent,dreszcze).
+podejrzenie(grypa) :-
+        ma_objaw(goraczka),
+        ma_objaw(bol_gardla),
+        ma_objaw(bol_glowy),
+        ma_objaw(bol_miesni),
+        ma_objaw(dreszcze),
+        ma_objaw(katar),
+        ma_objaw(kaszel).
 
-podejrzenie(Pacjent,szkarlatyna) :-
-        ma_objaw(Pacjent,bol_gardla),
-        ma_objaw(Pacjent,wysypka),
-        ma_objaw(Pacjent,goraczka),
-        ma_objaw(Pacjent,biegunka),
-        ma_objaw(Pacjent,obrzek_wezlow_chlonnych).
+podejrzenie(przeziebienie) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(bol_gardla),
+        ma_objaw(kichanie),
+        ma_objaw(katar),
+        ma_objaw(dreszcze).
 
-podejrzenie(Pacjent,grypa_zoladkowa) :-
-        ma_objaw(Pacjent,bol_brzucha),
-        ma_objaw(Pacjent,wymioty),
-        ma_objaw(Pacjent,goraczka),
-        ma_objaw(Pacjent,bol_glowy),
-        ma_objaw(Pacjent,biegunka).
+podejrzenie(szkarlatyna) :-
+        ma_objaw(bol_gardla),
+        ma_objaw(wysypka),
+        ma_objaw(goraczka),
+        ma_objaw(biegunka),
+        ma_objaw(obrzek_wezlow_chlonnych).
 
-podejrzenie(Pacjent,nadcisnienie) :-
-        ma_objaw(Pacjent,bol_glowy),
-        ma_objaw(Pacjent,zmeczenie),
-        ma_objaw(Pacjent,nerwowosc),
-        ma_objaw(Pacjent,dusznosci),
-        ma_objaw(Pacjent,lomotanie_serca).
+podejrzenie(grypa_zoladkowa) :-
+        ma_objaw(bol_brzucha),
+        ma_objaw(wymioty),
+        ma_objaw(goraczka),
+        ma_objaw(bol_glowy),
+        ma_objaw(biegunka).
 
-podejrzenie(Pacjent,rozyczka) :-
-        ma_objaw(Pacjent,bol_glowy),
-        ma_objaw(Pacjent,wysypka),
-        ma_objaw(Pacjent,goraczka),
-        ma_objaw(Pacjent,katar),
-        ma_objaw(Pacjent,biegunka).
+podejrzenie(nadcisnienie) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(zmeczenie),
+        ma_objaw(nerwowosc),
+        ma_objaw(dusznosci),
+        ma_objaw(lomotanie_serca).
+
+podejrzenie(rozyczka) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(wysypka),
+        ma_objaw(goraczka),
+        ma_objaw(katar),
+        ma_objaw(biegunka).
+
+ma_objaw(X) :- xpozytywne(X), !.
+ma_objaw(X) :- \+xnegatywne(X), pytaj(X, tak).
+
+pytaj(X, tak) :- !, format('Czy ma Pan/Pani ~w ? (t/n)~n',[X]),
+                    read_line_to_codes(user_input,MenuCodes),
+                    string_codes(MyString,MenuCodes),
+                    atom_number(MyString, Reply),
+                    pamietaj(X, Reply).
+
+wyczysc_fakty :- write('Przycisnij cos aby wyjsc'), nl,
+                    retractall(xpozytywne(_)),
+                    retractall(xnegatywne(_)),
+                    get_char(_).
+
+pamietaj(X,'t') :- assertz(xpozytywne(X)).
+pamietaj(X,'n') :- assertz(xnegatywne(X)).
+
+start :- podejrzenie(X), !,
+            format('~ chory na ~w', X),
+            nl, wyczysc_fakty.
+            
+start :- write('Nie jestem w stanie odgadnac co to za choroba.'), nl,
+            wyczysc_fakty.
