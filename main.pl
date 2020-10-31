c8393ef374aaefeb015a3c531462682f21c43372
greg9702 <grzegorzb9702@gmail.com>
2020-10-31 17:59:56 +0100

    Removed redundant file main

--- main.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(library(dcg/basics)).
-:-(clause(zawodnik(_,_),_); consult('db.pl')).
-
-menu:-
-    read_line_to_codes(user_input,MenuCodes),
-    string_codes(String, MenuCodes),
-    atom_number(String, Selected),
-    write(Selected).
