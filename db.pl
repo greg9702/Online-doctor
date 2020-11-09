6f25ff788fb5c6e34938b83027bf8cb57779aa0f
greg9702 <grzegorzb9702@gmail.com>
2020-11-09 12:27:00 +0100

    Added previously removed diseases

--- db.pl
+++ db.pl
@@ -7,19 +7,129 @@ podejrzenie(grypa) :-
         ma_objaw(katar),
         ma_objaw(kaszel).
 
-podejrzenie(rozyczka) :-
+podejrzenie(przeziebienie) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(bol_gardla),
+        ma_objaw(kichanie),
+        ma_objaw(katar),
+        ma_objaw(dreszcze).
+
+podejrzenie(szkarlatyna) :-
+        ma_objaw(bol_gardla),
+        ma_objaw(wysypka),
         ma_goraczke(wysoka_goraczka),
-        typ(rozyczka, zakaźna), % TODO make this is optional
-        ryzyko_ze_wzgledu(wiek),
+        ma_objaw(biegunka),
+        ma_objaw(obrzek_wezlow_chlonnych).
+
+podejrzenie(grypa_zoladkowa) :-
+        ma_objaw(bol_brzucha),
+        ma_objaw(wymioty),
+        ma_goraczke(goraczka),
+        ma_objaw(bol_glowy),
+        ma_objaw(biegunka).
+
+podejrzenie(nadcisnienie) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(zmeczenie),
+        ma_objaw(nerwowosc),
+        ma_objaw(dusznosci),
+        ma_objaw(lomotanie_serca),
+        ryzyko_ze_wzgledu(wiek).
+
+podejrzenie(rozyczka) :-
         ma_objaw(bol_glowy),
         ma_objaw(wysypka),
-        ma_objaw(goraczka),
+        ma_goraczke(goraczka),
         ma_objaw(katar),
         ma_objaw(biegunka).
+
+podejrzenie(goraczka_krwotoczna) :-
+        ma_objaw(bol_glowy),
+        ma_objaw(bol_brzucha),
+        ma_objaw(bol_miesni),
+        ma_objaw(obrzeki),
+        ma_objaw(krawienie_blon_sluzowych).
+
+podejrzenie(goraczka_reumatyczna) :-
+        ma_objaw(bol_stawow),
+        ma_objaw(wysokie_OB),
+        ma_objaw(leukocytoza),
+        ma_objaw(wyoskie_CRP),
+        ma_objaw(gozki_podskorne),
+        ryzyko_ze_wzgledu(mlody_wiek).
+
+podejrzenie(bolerioza) :-
+        ma_objaw(nudnosci),
+        ma_objaw(wymioty),
+        ma_objaw(uposledzenie_sluchu),
+        ma_objaw(padaczka),
+        ma_objaw(psychoza).
+
+podejrzenie(mukowiscydoza) :-
+        ma_objaw(kaszel),
+        ma_objaw(dusznosci),
+        ma_objaw(polipy_nosa),
+        ma_objaw(obfite_stolce),
+        ma_objaw(wypadanie_odbytu).
+
+podejrzenie(mocznica) :-
+        ma_objaw(drazliwosc),
+        ma_objaw(sennosc),
+        ma_objaw(pobudliwosc),
+        ma_objaw(brak_laknienia),
+        ma_objaw(wybroczyny).
+
+podejrzenie(odra) :-
+        ma_objaw(niezyt_nosa),
+        ma_objaw(zapalenie_spojowek),
+        ma_objaw(plamki_koplika),
+        ma_objaw(sinica),
+        ma_objaw(przyspieszenie_tetna).
+
+podejrzenie(tezec) :-
+        ma_objaw(zaburzenia_czucia),
+        ma_objaw(niepokoj),
+        ma_objaw(skurcze_miesni),
+        ma_objaw(drgawki),
+        ma_objaw(dusznosc).
+
+podejrzenie(ptasia_grypa) :-
+        ma_objaw(zapalenie_spojowek),
+        ma_objaw(wymioty),
+        ma_objaw(bol_gardla),
+        ma_objaw(biegunka),
+        ma_objaw(kaszel).
+
+podejrzenie(polio) :-
+        ma_objaw(parestezje),
+        ma_objaw(porazenie_miesni),
+        ma_objaw(sztywnosc_karku),
+        ma_goraczke(goraczka),
+        ma_objaw(bol_glowy).
+
+podejrzenie(ospa_wietrzna) :-
+        ma_objaw(swedzenie),
+        ma_objaw(wykwity),
+        ma_objaw(bol_miesni),
+        ma_goraczke(goraczka),
+        ma_objaw(biegunka).
+
+podejrzenie(gruzlica) :-
+        ma_objaw(poty_nocne),
+        ma_objaw(stan_podgoraczkowy),
+        ma_objaw(krwioplucie),
+        ma_objaw(utrata_masy_ciala),
+        ma_objaw(oslabienie),
+        ryzyko_ze_wzgledu(wiek).
+
 
 ma_goraczke(wysoka_goraczka) :-
         zmierzona_temperatura(X),
         X > 38.
+
+ma_goraczke(goraczka) :-
+        zmierzona_temperatura(X),
+        X > 37.
 
 ma_objaw(Val):- 
         nl, zadaj_pytanie('Czy ma Pan/Pani ', Val).
@@ -27,6 +137,10 @@ ma_objaw(Val):-
 ryzyko_ze_wzgledu(wiek):-
         wiek_pacjenta(X),
         X > 50.
+
+ryzyko_ze_wzgledu(mlody_wiek):-
+        wiek_pacjenta(X),
+        X < 30.
 
 typ(NazwaChoroby, zakaźna) :-
         nl, zadaj_pytanie('Czy miał/a Pan/Pani kontakt w ostanim czasie z osobą chorą ', NazwaChoroby).
